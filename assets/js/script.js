ab25180819edc5738c8c99db01d485673e9d4ac9
WarrenVermelle <warren.vermelle@outlook.com>
2023-10-17 14:15:52 +0200

    feature: call api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
 const form = document.getElementsByClassName("form-search");
 const button = document.getElementsByClassName("btn-search");
 
-button[0].addEventListener("click", function(){
+button[0].addEventListener("click", async function(){
     const element = document.getElementById("choices-text-preset-values");
-    console.log(element.value);
+
+    const response = await fetch("https://127.0.0.1:3000/ask", {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(element.value),
+    });
+
+    console.log(response);
 });
